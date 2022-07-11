1fad1b959cea523e5e042a73062bbbede1b20688
yohan.molina <yohan.molina@novaip.com.co>
2022-07-11 17:17:24 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
